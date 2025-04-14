e77efe94e8c95a7b88e821126aa7210146d7f5c5
OmniaUK <Harry@Bowker.biz>
2025-04-14 21:21:58 +0100

    Working camera prototype, Maybe? It builds lmao

--- src/go1_ros2_cpp/src/stereo_camera.cpp
+++ src/go1_ros2_cpp/src/stereo_camera.cpp
@@ -19,59 +19,56 @@
 // PHS: Modified topic name used
 // HAB75: Modified to stereo_camera nodes for Unitree Go1
 
-//https://en.cppreference.com/w/cpp/chrono
+// https://en.cppreference.com/w/cpp/chrono
 #include <chrono> // provides system_clock, times and durations
 
-//https://en.cppreference.com/w/cpp/header/functional
+// https://en.cppreference.com/w/cpp/header/functional
 #include <functional> // provides the std::bind()
 
-//https://en.cppreference.com/w/cpp/header/memory
+// https://en.cppreference.com/w/cpp/header/memory
 #include <memory> // provides std::make_shared<Class>()
 
-//https://en.cppreference.com/w/cpp/header/string
+// https://en.cppreference.com/w/cpp/header/string
 #include <string> // provides std::to_string()
 
-//ROS 2 Specific includes
-//https://docs.ros2.org/latest/api/rclcpp/rclcpp_8hpp.html
+// ROS 2 Specific includes
+// https://docs.ros2.org/latest/api/rclcpp/rclcpp_8hpp.html
 #include "rclcpp/rclcpp.hpp" // includes most common libraries for ROS 2
-//https://index.ros.org/p/std_msgs/
-//https://docs.ros.org/en/humble/Concepts/Basic/About-Interfaces.html
+// https://index.ros.org/p/std_msgs/
+// https://docs.ros.org/en/humble/Concepts/Basic/About-Interfaces.html
 #include "sensor_msgs/msg/point_cloud2.hpp" // Point cloud message from cameras
 
-//Important: these includes should be reflected in the package.xml and CMakeLists.txt
+// Important: these includes should be reflected in the package.xml and CMakeLists.txt
 #include "UnitreeCameraSDK.hpp"
 
-//Namespace specified for simplfication when using chrono library
+// Namespace specified for simplfication when using chrono library
 using namespace std::chrono_literals;
 using namespace cv; // Open CV namespace, Added for same reasons above.
 
-// UnitreeCamera StereoCamera
-UnitreeCamera camera("../sdk/UnitreecameraSDK/stereo_camera_config.yaml"); //Unitree Go1 head-front camera
-
-
-
-
+// UnitreeCamera StereoCamera object
+UnitreeCamera camera("../sdk/UnitreecameraSDK/stereo_camera_config.yaml"); // Unitree Go1 head-front camera
 
 /* This program creates a node for outputting image data from the Go1 stereo cameras
    That will include point cloud data & raw image data */
 
-//Declaring a new class as a subclass of the ROS 2 Node class
+// Declaring a new class as a subclass of the ROS 2 Node class
 class CameraPublisher : public rclcpp::Node
 {
 public:
-  //Constructor, specifying node name and initialising a counter
+  // Constructor, specifying node name and initialising a counter
   CameraPublisher()
-  : Node("CameraSDK"), count_(0)
+      : Node("CameraSDK"), count_(0)
   {
+
     // Create the instance of the publisher that will publish messages
     // of type std_msgs/mgs/UInt32 to the topic "/hello/world"
     // a queue length of 10 is specified here for the topic
-    publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/sensors/camera/face_camera/image_pointcloud", 10);
+    publisher_PCPrototype = this->create_publisher<sensor_msgs::msg::PointCloud2>("/sensors/camera/face_camera/image_pointcloud", 10);
 
     // Create a timer that will trigger calls to the method timer_callback
     // every 0.5s
     timer_ = this->create_wall_timer(
-      500ms, std::bind(&CameraPublisher::timer_callback, this));
+        100ms, std::bind(&CameraPublisher::timer_callback, this));
   }
 
 private:
@@ -79,38 +76,94 @@ private:
   // at the specified intervals
   void timer_callback()
   {
-    //Create an instance of the UInt32 message type
+    // Create an instance of the UInt32 message type
     auto message = sensor_msgs::msg::PointCloud2();
 
-    //Set the pre-defined field "data" in the message to a positive integer value,
-    // incrementing the counter each time a message is constructed
-    message.;
+    // SDK PointCloud raw data with points
+    std::vector<PCLType> pointCloud_RAW;
+    // Raw data timestamp
+    std::chrono::microseconds timestamp;
+    // Capture raw data
+    try
+    {
+      camera.getPointCloud(pointCloud_RAW, timestamp);
+    } // If theres an error, Throw error to ros output terminal
+    catch (const std::exception &e)
+    {
+      RCLCPP_ERROR(this->get_logger(), "Failed to get point cloud: %s", e.what());
+    }
+    catch (...)
+    {
+      RCLCPP_ERROR(this->get_logger(), "Unknown error occurred while getting point cloud.");
+    }
 
-    //Add ROS 2 logger entry with details of the message (print message to console)
-    //RCLCPP_INFO(this->get_logger(), "Publishing: msg.data=%i", message.data);
 
-    //publish the message created above to the topic /hello/world
-    publisher_->publish(message);
+    // set timestamp from ros internal clock
+    // source from: https://docs.ros.org/en/foxy/Tutorials/Intermediate/Tf2/Learning-About-Tf2-And-Time-Cpp.html
+    message.header.stamp = this->get_clock()->now();
+    message.header.frame_id = "face_camera";
+
+    // // Set dimensions
+    // message.width = 928; // From YAML FrameSize. No idea how to have this set by the YAML file..
+    // message.height = 400;
+    // message.is_dense = false;
+    // message.is_bigendian = false;
+
+    message.height = 1;
+    message.width = pointCloud_RAW.size();
+    message.is_dense = false;
+    message.is_bigendian = false;
+    message.point_step = 16;
+    message.row_step = message.point_step * message.width;
+    message.data.resize(message.row_step);
+
+    uint8_t *ptr = message.data.data();
+    for (const auto &pt : pointCloud_RAW)
+    {
+      float x = pt.pts[0];
+      float y = pt.pts[1];
+      float z = pt.pts[2];
+      uint8_t r = pt.clr[2];
+      uint8_t g = pt.clr[1];
+      uint8_t b = pt.clr[0];
+
+      uint32_t rgb_packed = (r << 16) | (g << 8) | (b);
+      float rgb_float;
+      std::memcpy(&rgb_float, &rgb_packed, sizeof(float));
+
+      std::memcpy(ptr, &x, sizeof(float));
+      std::memcpy(ptr + 4, &y, sizeof(float));
+      std::memcpy(ptr + 8, &z, sizeof(float));
+      std::memcpy(ptr + 12, &rgb_float, sizeof(float));
+
+      ptr += message.point_step;
+    }
+
+    // Add ROS 2 logger entry with details of the message (print message to console)
+    // RCLCPP_INFO(this->get_logger(), "Publishing: msg.data=%i", message.data);
+
+    // publish the message created above to the topic /hello/world
+    publisher_PCPrototype->publish(message);
   }
 
-  //Declaration of private fields used for timer, publisher and counter
+  // Declaration of private fields used for timer, publisher and counter
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_PCPrototype;
   size_t count_;
 };
 
-//Main method defining entry point for program
-int main(int argc, char * argv[])
+// Main method defining entry point for program
+int main(int argc, char *argv[])
 {
-  //Initialise ROS 2 for this node
+  // Initialise ROS 2 for this node
   rclcpp::init(argc, argv);
 
-  //Create the instance of the Node subclass and 
-  // start the spinner with a pointer to the instance
-  // This will keep the node running until interupted by ROS or node returns
+  // Create the instance of the Node subclass and
+  //  start the spinner with a pointer to the instance
+  //  This will keep the node running until interupted by ROS or node returns
   rclcpp::spin(std::make_shared<CameraPublisher>());
 
-  //When the node is terminated, shut down ROS 2 for this node
+  // When the node is terminated, shut down ROS 2 for this node
   rclcpp::shutdown();
   return 0;
 }
